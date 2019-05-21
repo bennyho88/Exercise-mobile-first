4615a46a8ccb31d3a6c04553d1ce3dc0e11982d4
Benny Ho <benny.ho2530@gmail.com>
2019-05-21 14:41:10 +0200

    added css content and js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
 // Animate Smooth Scroll 
-$('#view-work'.on('click', function()) {
-    const images = $('images').position().top;
+$('#view-work').on('click', function() {
+    const images = $('#images').position().top;
 
-    $('html, body').animate( {
+    $('html, body').animate( 
+    {
         scrollTop: images
-    }, 900);
+    }, 
+    900
+    );
 });
